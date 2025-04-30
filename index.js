dfcfb0aa82ef7ca5977dbcc0ffaf4b69be941b52
Christie <roy.christie95@gmail.com>
2025-04-30 10:58:23 -0500

    add Osano API call back to indexjs

--- index.js
+++ index.js
@@ -1,70 +1,58 @@
-// // Function to call the Osano JavaScript API & allow banner suppression
+// OSANO JS API function to hide banner in unwanted locales
+(function (w, o, d) {
+  w[o] =
+    w[o] ||
+    function () {
+      w[o][d].push(arguments);
+    };
+  w[o][d] = w[o][d] || [];
+})(window, 'Osano', 'data');
 
-fetch('https://ipinfo.io/json?token=09a57283894e52')
-  .then(response => response.json())
-  .then(data => {
-    const stateName = data.region || '';
-    const country = data.country || '';
-    const normalizedState = `${country}-${stateName.toUpperCase().replace(/\s/g, '_')}`; // e.g., US-CALIFORNIA
-    console.log('Detected region:', normalizedState);
+const allowedJurisdictions = [
+  'us-ca', 'us-co', 'us-ct', 'us-va', 'us-ut',
+  'us-tx', 'us-or', 'us-mt', 'us-ia', 'us-nh',
+  'us-de', 'us-nj', 'us-ne'
+];
 
-    const allowedRegions = [
-      'US-CALIFORNIA', 'US-COLORADO', 'US-CONNECTICUT', 'US-VIRGINIA', 'US-UTAH',
-      'US-TEXAS', 'US-OREGON', 'US-MONTANA', 'US-IOWA', 'US-NEW_HAMPSHIRE',
-      'US-DELAWARE', 'US-NEW_JERSEY', 'US-NEBRASKA'
-    ];
-
-    if (allowedRegions.includes(normalizedState)) {
-      // Preload Osano
-      (function (w, o, d) {
-        w[o] = w[o] || function () { w[o][d].push(arguments); };
-        w[o][d] = w[o][d] || [];
-      })(window, 'Osano', 'data');
-
-      // Load Osano
-      const osanoScript = document.createElement('script');
-      osanoScript.src = 'https://cmp.osano.com/AzZcpvRm9bbsqngN/5cd0582e-bdad-4a5c-8484-356e17bbbe1e/osano.js?variant=two';
-      osanoScript.async = true;
-      document.head.appendChild(osanoScript);
-    } else {
-      console.log('Osano not loaded for this region.');
+window.Osano('onInitialized', function () {
+  setTimeout(function () {
+    const jurisdiction = (window.Osano.cm.jurisdiction || '').toLowerCase();
+    console.log('Osano jurisdiction detected:', jurisdiction);
+    if (!allowedJurisdictions.includes(jurisdiction)) {
+      window.Osano.cm.hideDialog();
     }
-  })
-  .catch(err => {
-    console.warn('Could not determine location. Not loading Osano.', err);
-  });
+  }, 300); // Allow time for jurisdiction to populate
+});
 
 
-// // index.js
-// const allowedJurisdictions = [
-//   'US-CA', 'US-CO', 'US-CT', 'US-VA', 'US-UT',
-//   'US-TX', 'US-OR', 'US-MT', 'US-IA', 'US-NH',
-//   'US-DE', 'US-NJ', 'US-NE'
-// ];
+// // Function to call the Osano JavaScript API & allow banner suppression THIRD PARTY API
 
-// // Use a third-party IP geolocation API
 // fetch('https://ipinfo.io/json?token=09a57283894e52')
 //   .then(response => response.json())
 //   .then(data => {
-//     const regionCode = `${data.country}-${data.region_code}`.toUpperCase(); // e.g., "US-CA"
-//     console.log('Detected region:', regionCode);
+//     const stateName = data.region || '';
+//     const country = data.country || '';
+//     const normalizedState = `${country}-${stateName.toUpperCase().replace(/\s/g, '_')}`; // e.g., US-CALIFORNIA
+//     console.log('Detected region:', normalizedState);
 
-//     if (allowedJurisdictions.includes(regionCode)) {
-//       // Insert Osano script dynamically
+//     const allowedRegions = [
+//       'US-CALIFORNIA', 'US-COLORADO', 'US-CONNECTICUT', 'US-VIRGINIA', 'US-UTAH',
+//       'US-TEXAS', 'US-OREGON', 'US-MONTANA', 'US-IOWA', 'US-NEW_HAMPSHIRE',
+//       'US-DELAWARE', 'US-NEW_JERSEY', 'US-NEBRASKA'
+//     ];
+
+//     if (allowedRegions.includes(normalizedState)) {
+//       // Preload Osano
+//       (function (w, o, d) {
+//         w[o] = w[o] || function () { w[o][d].push(arguments); };
+//         w[o][d] = w[o][d] || [];
+//       })(window, 'Osano', 'data');
+
+//       // Load Osano
 //       const osanoScript = document.createElement('script');
 //       osanoScript.src = 'https://cmp.osano.com/AzZcpvRm9bbsqngN/5cd0582e-bdad-4a5c-8484-356e17bbbe1e/osano.js?variant=two';
 //       osanoScript.async = true;
 //       document.head.appendChild(osanoScript);
-
-//       // Set up Osano pre-load
-//       (function (w, o, d) {
-//         w[o] =
-//           w[o] ||
-//           function () {
-//             w[o][d].push(arguments);
-//           };
-//         w[o][d] = w[o][d] || [];
-//       })(window, 'Osano', 'data');
 //     } else {
 //       console.log('Osano not loaded for this region.');
 //     }
